85fe4471fe315fd92f90ba51255541f82448a28a
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-06 14:13:50 -0300

    arregle errores de ruta

--- src/Paginas/AboutUs/AboutUs.jsx
+++ src/Paginas/AboutUs/AboutUs.jsx
@@ -1,34 +1,9 @@
 import React from 'react'
-import { Container, Row, Col, Image,card } from 'react-bootstrap';
+import { Container, Row, Col, Image, Card } from 'react-bootstrap';
 
 const AboutUs = () => {
   return (
-   <div> 
-     <Container>
-     <Row className="mb-4">
-          <Col>
-            <h1 className="text-center">Acerca de Nosotros</h1>
-          </Col>
-        </Row>
-        <Row className="mb-4">
-          <Col>
-            <p className="text-center">
-              Breve descripción de la misión de la empresa.
-            </p>
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col xs={12} md={6} lg={3} className="mb-4">
-            <Card className="text-center">
-              <Card.Img
-                variant="top"
-                src="/ruta-de-la-imagen-integrante-1.jpg"
-                alt="Integrante 1"
-                className="rounded-circle mt-3"
-              />
-     </Container>
-
-   </div>
+   <p>abooout</p>
   )
 }
 
