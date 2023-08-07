16f29da3aab4c6fdbfbc45bf69794b130581a1aa
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-06 21:42:51 -0300

    se realizaron correciones de estilos

--- src/Paginas/AboutUs/AboutUs.jsx
+++ src/Paginas/AboutUs/AboutUs.jsx
@@ -1,60 +1,98 @@
 import React from 'react';
-import { Container, Row, Col, Image, Card } from 'react-bootstrap';
+import { Container, Image, Card, Row, Col } from 'react-bootstrap';
 
 const AboutUs = () => {
   return (
     <div className='AboutUs'>
       {/* Banner */}
       <div className="banner bg-success d-flex align-items-center justify-content-center">
-        <Image src="/public/logo_RHR.jpg" alt="Banner" className="img-fluid banner-image" />
+        <Image src="/public/logo_RHR.jpg" alt="Banner" className="img-fluid banner-image" style={{ maxWidth: '20%', height: 'auto' }} />
       </div>
       <Container>
-        <div className="row text-bg text-center my-4">
-          <div className="col-md-8">
-            <h1>
-              ¡Bienvenidos a Rolling Hotel&Resort! Somos un equipo de jóvenes programadores apasionados que creamos una página de gestión hotelera. Nuestra plataforma ofrece un servicio de autogestión y reservas para el público general. Simplifica tus operaciones y mejora la experiencia de tus clientes con Rolling Hotel&Resort.
+      <Row className="text-center my-4">
+          <Col md={{ span: 8, offset: 2 }}>
+            <h1 className="display-4">
+              ¡Bienvenidos a Rolling Hotel&Resort!
             </h1>
+            <p className="lead">
+              Somos un equipo de jóvenes y apasionados programadores  que creamos y desarrollamos una página de gestión hotelera. Nuestra plataforma ofrece un servicio de autogestión y reservas para el público general. Simplifica tus operaciones y mejora la experiencia de tus clientes con Rolling Hotel&Resort.
+            </p>
+          </Col>
+        </Row>
+
+        <div className="row text-center mt-3">
+          <div className="col-12">
+            <br />
+            <hr />
+
+            <h3 className="mb-4 mt-6">Conoce a nuestro equipo</h3>
           </div>
         </div>
 
-        <Row className="mb-4">
-          {/* Cards para los nombres de cada integrante */}
-          <Col md={3} sm={6} className="mb-4">
-            <Card className="shadow-sm border-0">
-              <Card.Img variant="top" src="/ruta-de-la-imagen-integrante-1.jpg" />
+        {/* Cards de los integrantes */}
+        <Row className="justify-content-between mb-4">
+          <Col md={5} className="mb-4">
+            <Card className="shadow">
+              <Card.Img
+                src="https://media.istockphoto.com/id/1384874531/es/vector/silueta-de-hombre-de-traje-avatar-no-identificado.jpg?s=612x612&w=0&k=20&c=9vuS9E6RA0ZM0oOSxD0zVaxc6DvDwwDCrtYFJrq4sMU="
+                alt="Juan Ignacio Luquez"
+                className="card-img-top"
+                style={{ maxWidth: '80%', height: 'auto' }}
+              />
               <Card.Body>
-                <Card.Title className="text-center">Nombre del Integrante 1</Card.Title>
+                <Card.Title>Juan Ignacio Luquez</Card.Title>
               </Card.Body>
             </Card>
           </Col>
-          <Col md={3} sm={6} className="mb-4">
-            <Card className="shadow-sm border-0">
-              <Card.Img variant="top" src="/ruta-de-la-imagen-integrante-2.jpg" />
+          <Col md={5} className="mb-4">
+            <Card className="shadow">
+              <Card.Img
+                src="https://media.istockphoto.com/id/1384874531/es/vector/silueta-de-hombre-de-traje-avatar-no-identificado.jpg?s=612x612&w=0&k=20&c=9vuS9E6RA0ZM0oOSxD0zVaxc6DvDwwDCrtYFJrq4sMU="
+                alt="Luis Fabian Alvarez"
+                className="card-img-top"
+                style={{ maxWidth: '80%', height: 'auto' }}
+              />
               <Card.Body>
-                <Card.Title className="text-center">Nombre del Integrante 2</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={3} sm={6} className="mb-4">
-            <Card className="shadow-sm border-0">
-              <Card.Img variant="top" src="/ruta-de-la-imagen-integrante-3.jpg" />
-              <Card.Body>
-                <Card.Title className="text-center">Nombre del Integrante 3</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={3} sm={6} className="mb-4">
-            <Card className="shadow-sm border-0">
-              <Card.Img variant="top" src="/ruta-de-la-imagen-integrante-4.jpg" />
-              <Card.Body>
-                <Card.Title className="text-center">Nombre del Integrante 4</Card.Title>
+                <Card.Title>Luis Fabian Alvarez</Card.Title>
               </Card.Body>
             </Card>
           </Col>
         </Row>
+
+        <Row className="justify-content-between">
+          <Col md={5} className="mb-4">
+            <Card className="shadow">
+              <Card.Img
+                src="https://media.istockphoto.com/id/1384874531/es/vector/silueta-de-hombre-de-traje-avatar-no-identificado.jpg?s=612x612&w=0&k=20&c=9vuS9E6RA0ZM0oOSxD0zVaxc6DvDwwDCrtYFJrq4sMU="
+                alt="Ana Gallardini"
+                className="card-img-top"
+                style={{ maxWidth: '80%', height: 'auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Ana Gallardini</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={5} className="mb-4">
+            <Card className="shadow">
+              <Card.Img
+                src="https://media.istockphoto.com/id/1384874531/es/vector/silueta-de-hombre-de-traje-avatar-no-identificado.jpg?s=612x612&w=0&k=20&c=9vuS9E6RA0ZM0oOSxD0zVaxc6DvDwwDCrtYFJrq4sMU="
+                alt="Nicolas"
+                className="card-img-top"
+                style={{ maxWidth: '80%', height: 'auto' }}
+              />
+              <Card.Body>
+                <Card.Title>Nicolas</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
       </Container>
     </div>
   )
 }
 
 export default AboutUs;
+
+
