549ed03ec95cdf13a6de9ef56523aa280ca202b3
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-11 10:36:47 -0300

    agregue campos al formulario

--- src/Componentes/ContactoClientes/ContactoClientes.jsx
+++ src/Componentes/ContactoClientes/ContactoClientes.jsx
@@ -1,33 +1,50 @@
 import React from 'react';
-import { Form , InputGroup } from 'react-bootstrap';
+import { Form, InputGroup, FloatingLabel, Col, Row } from 'react-bootstrap';
 
 
 const ContactoClientes = () => {
-  return ( <>
-   <div className='d-flex justify-content-center'>
+  return (<>
+    <div className='d-flex justify-content-center'>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
-          <InputGroup size="lg">
-        <InputGroup.Text id="inputGroup-sizing-lg">Nombre Y Apellido </InputGroup.Text>
-        <Form.Control
-          aria-label="Large"
-          aria-describedby="inputGroup-sizing-sm"
-        />
-      </InputGroup>
+          <Form>
+            <br />
+            <Row>
+              <Col>
+                <Form.Control placeholder="Nombre y Apellido" rows={6} />
+              </Col>
+              <Col>
+                <Form.Control placeholder="Email" rows={6} />
+              </Col>
+            </Row>
+            <br />
+
+            <Form.Group className="mb-3" controlId="Teléfono">
+
+              <Form.Control placeholder="Teléfono" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="Motivo">
+
+              <Form.Control placeholder="Motivo" />
+            </Form.Group>
+
+          </Form>
+
+
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
+
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
-       
+
       </Form>
     </div>
-  
-  
+
+
   </>
-   
-)};
+
+  )
+};
 
 export default ContactoClientes;
