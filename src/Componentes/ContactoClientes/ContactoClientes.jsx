09e61d86d88d52d44d12db2c7a4796b5727a86b7
Juan Ignacio Luquez <juaning2023@gmail.com>
2023-08-11 09:47:51 -0300

    agregue campo de escribir nombre

--- src/Componentes/ContactoClientes/ContactoClientes.jsx
+++ src/Componentes/ContactoClientes/ContactoClientes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form , InputGroup } from 'react-bootstrap';
+
 
 const ContactoClientes = () => {
   return ( <>
@@ -8,11 +9,19 @@ const ContactoClientes = () => {
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
+          <InputGroup size="lg">
+        <InputGroup.Text id="inputGroup-sizing-lg">Nombre Y Apellido </InputGroup.Text>
+        <Form.Control
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </InputGroup>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Example textarea</Form.Label>
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
+       
       </Form>
     </div>
   
