a7cc55a55726922764e7e4d61347f22839852a6e
Luis Fabian Alvarez <127775354+LFALVAREZ84@users.noreply.github.com>
2023-08-21 14:14:16 -0300

    Update Rutas.jsx

--- src/Rutas/Rutas.jsx
+++ src/Rutas/Rutas.jsx
@@ -1,15 +1,31 @@
 import {Routes, Route } from 'react-router-dom'
 import InicioDeSesion from '../Componentes/InicioDeSesion/InicioDeSesion'
 import Registro from '../Componentes/Registro/Registro'
+import Formulario from '../Componentes/Reserva/Formulario'
+import Inventario from '../Componentes/Administrador/Inventario/Inventario'
+import Adeministrador from './../Componentes/Administrador/Administrador'
+import Habitaciones from '../Paginas/CatalogoDeHabitaciones/Habitaciones'
+import Descripcion from '../Paginas/CatalogoDeHabitaciones/Descripcion/Descripcion'
+import Carrrusel from '../Componentes/Carrusel/Carrusel'
+import Inicio from '../Paginas/PaginaPrincial/Inicio'
+import ContactoClientes from '../Paginas/Contacto/ContactoClientes'
+import Nosotros from '../Paginas/Nosotros/Nosotros'
 
 const Rutas = () => {
   return (
     <>
     <Routes>
-      <Route path='/' element={<></>} />
+      <Route path='/' element={<Inicio />} />
       <Route path="/InicioDeSesion" element={<InicioDeSesion />} />
       <Route path="/Registro" element={<Registro />} />
-      
+      <Route path="/" element={<Formulario />} />
+      <Route path="/Inventario" element={<Inventario />} />  {/* Hacer una ruta privada*/}
+      <Route path="/Administrador" element={<Adeministrador />} />  {/* Hacer una ruta privada*/}
+      <Route path="/Habitaciones" element={<Habitaciones />} />
+      <Route path="/Descripcion" element={<Descripcion />} />
+      <Route path="/Carusel" element={<Carrrusel />} />
+      <Route path="/ContactoClientes" element={<ContactoClientes />} />
+      <Route path="/Nosotros" element={<Nosotros />} />
     </Routes>
     </>
   )
