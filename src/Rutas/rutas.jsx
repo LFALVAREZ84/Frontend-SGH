a112b6f530eec91e129312c79eb18294e1d06597
Luis Fabian Alvarez <127775354+LFALVAREZ84@users.noreply.github.com>
2023-08-21 14:10:03 -0300

    Panel de Administrador

--- src/Rutas/rutas.jsx
+++ src/Rutas/rutas.jsx
@@ -3,6 +3,7 @@ import InicioDeSesion from '../Componentes/InicioDeSesion/InicioDeSesion'
 import Registro from '../Componentes/Registro/Registro'
 import Formulario from '../Componentes/Reserva/Formulario'
 import Inventario from '../Componentes/Administrador/Inventario/Inventario'
+import Adeministrador from './../Componentes/Administrador/Administrador'
 import Habitaciones from '../Paginas/CatalogoDeHabitaciones/Habitaciones'
 import Descripcion from '../Paginas/CatalogoDeHabitaciones/Descripcion/Descripcion'
 import Carrrusel from '../Componentes/Carrusel/Carrusel'
@@ -19,6 +20,7 @@ const Rutas = () => {
       <Route path="/Registro" element={<Registro />} />
       <Route path="/" element={<Formulario />} />
       <Route path="/Inventario" element={<Inventario />} />  {/* Hacer una ruta privada*/}
+      <Route path="/Administrador" element={<Adeministrador />} />  {/* Hacer una ruta privada*/}
       <Route path="/Habitaciones" element={<Habitaciones />} />
       <Route path="/Descripcion" element={<Descripcion />} />
       <Route path="/Carusel" element={<Carrrusel />} />
